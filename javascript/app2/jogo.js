a56835c07b9691803db302895c4be9a9a47aadf8
bernazyzz <zebernardomoniz@gmail.com>
2023-08-27 19:52:17 +0100

    mosquito game started

---
+++ javascript/app2/jogo.js
@@ -0,0 +1,32 @@
+var largura = 0
+var altura = 0
+
+function ajustarTamanhoDoJogo() {
+    largura = window.innerWidth
+    altura = window.innerHeight
+
+    console.log(largura, altura)
+}
+
+ajustarTamanhoDoJogo()
+
+function posicaoRandom() {
+
+    var posX = Math.floor(Math.random() * largura) - 90
+    var posY = Math.floor(Math.random() * altura) - 90
+
+    posX = posX < 0 ? 0 : posX
+    posY = posY < 0 ? 0 : posY
+    
+    console.log(posX, posY)
+    
+    // criar o elemnto html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posX + 'px'
+    mosquito.style.top = posY + 'px'
+    mosquito.style.position = 'absolute'
+    
+    document.body.appendChild(mosquito)
+}
