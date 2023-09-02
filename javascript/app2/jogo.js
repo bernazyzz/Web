f18458d39fc187db08bc8dbbc81415955219104a
bernazyzz <zebernardomoniz@gmail.com>
2023-09-02 16:21:06 +0100

    game finished

--- javascript/app2/jogo.js
+++ javascript/app2/jogo.js
@@ -1,5 +1,23 @@
 var largura = 0
 var altura = 0
+var vidas = 1
+var tempo = 15
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+    //1500
+    criaMosquitoTempo = 1500
+} else if (nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+} else if (nivel === 'nightmare') {
+    //750
+    criaMosquitoTempo = 750
+}
 
 function ajustarTamanhoDoJogo() {
     largura = window.innerWidth
@@ -10,11 +28,36 @@ function ajustarTamanhoDoJogo() {
 
 ajustarTamanhoDoJogo()
 
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+}, 1000)
+
 function posicaoRandom() {
 
     // remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')) {
-    document.getElementById('mosquito').remove()
+        document.getElementById('mosquito').remove()
+
+        // console.log('elemento selecionado foi: v' + vidas)
+        if (vidas > 3) {
+
+            window.location.href = 'fim_de_jogo.html'
+
+        } else {
+
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+
+        }
     }
 
     var posX = Math.floor(Math.random() * largura) - 90
@@ -33,6 +76,9 @@ function posicaoRandom() {
     mosquito.style.top = posY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
 }
